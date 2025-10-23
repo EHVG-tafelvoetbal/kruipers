e8564737069ae9965732e0faf9c7f26278af9a89
An "Rick" Truong <104770315+atehvg@users.noreply.github.com>
2025-10-23 16:38:20 +0200

    Add Stefan and Igor

--- data.js
+++ data.js
@@ -272,11 +272,11 @@ const data = {
         },
         10: {
             Mike: 2, // 02-10 (& Stefan) door Jelmer+Beau; // 20-10 (& Igor) door An+Beau;
-            Stefan: 3, // 02-10 (& Mike) door Jelmer+Beau; // 16-10 (& Marijn) door Ricardo+Boyd; // 16-10 (& Reno) door Lars (solo);
+            Stefan: 4, // 02-10 (& Mike) door Jelmer+Beau; // 16-10 (& Marijn) door Ricardo+Boyd; // 16-10 (& Reno) door Lars (solo); // 23-10 (& Igor) door Lars+Brandon
             Jelmer: 2, //06-10 (Hakbijl gooien) door Mike;
             Marijn: 1, // 16-10 (& Stefan) door Ricardo+Boyd;
             Reno: 1, // 16-10 (& Stefan) door Lars (solo);
-            Igor: 1, // 20-10 (& Mike) door An+Beau;
+            Igor: 2, // 20-10 (& Mike) door An+Beau; // 23-10 (& Stefan) door Lars+Brandon
         },
     },
 };
