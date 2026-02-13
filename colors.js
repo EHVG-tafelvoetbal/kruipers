56d9040a4d446e9f39ed98f4487b1682d26e8e3b
Ricardo van de Kruisweg <Rivadek@outlook.com>
2026-02-13 15:44:35 +0100

    fix: added better matching color for Jasper

--- colors.js
+++ colors.js
@@ -17,7 +17,7 @@ const colors = {
     An: "#47e933",
     Max: "#7190e3",
     Marijn: "#315700",
-    Jasper: "#719a30",
+    Jasper: "#DFFF00",
     Janoeska: "#ff458e",
     Shaldi: "#000",
     Brandon: "#935CCF",
