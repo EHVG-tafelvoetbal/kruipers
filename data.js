51107bea3565c2a48c0475144e1ac7d9d9ec91bf
An "Rick" Truong <104770315+atehvg@users.noreply.github.com>
2026-02-26 15:10:02 +0100

    Add Mike & Stefan

--- data.js
+++ data.js
@@ -310,12 +310,13 @@ const data = {
         },
         02: {
             Jasper: 3, // 04-02 (& Jelmer) door Sander+Lars; // 09-02 (& Stefan) door Ricardo en Lars; // 17-02 (& Marijn) door Beau & Mike
-            Jelmer: 4, // 04-02 (& Jasper) door Sander+Lars; // 04-02 (& Stefan) door Jasper+Lars; // 05-02 (& Ibrahim) door Beau+Reno; // 25-02 (& Sander) door Lars+Jasper;
-            Stefan: 4, // 04-02 (& Jelmer) door Jasper+Lars; // 09-02 (& Jasper) door Ricardo en Lars; // 16-02 (& Marijn) door Beau en Jasper; // 18-02 (& Ibrahim) door Lars & Ricardo;
+            Jelmer: 4, // 04-02 (& Jasper) door Sander+Lars; // 04-02 (& Stefan) door Jasper+Lars; // 05-02 (& Ibrahim) door Beau+Reno; // 25-02 (& Sander) door Lars+Jasper;  // 26-02 (& Mike) door Beau en Jelmer;
+            Stefan: 5, // 04-02 (& Jelmer) door Jasper+Lars; // 09-02 (& Jasper) door Ricardo en Lars; // 16-02 (& Marijn) door Beau en Jasper; // 18-02 (& Ibrahim) door Lars & Ricardo;
             Ibrahim: 2, // 05-02 (& Jelmer) door Beau+Reno; // 18-02 (& Stefan) door Lars & Ricardo;
             Rick: 1, // 13-02 (& Sander) door Lars+Ricardo;
             Sander: 2, // 13-02 (& Rick) door Lars+Ricardo; // 25-02 (& Jelmer) door Lars+Jasper;
             Marijn: 2, // 16-02 (& Stefan) door Beau en Jasper; // 17-02 (& Jasper) door Beau & Mike
+            Mike: 1,  // 26-02 (& Stefan) door Beau en Jelmer;
         },
     },
 };
