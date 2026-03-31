b6903c87daf20a4352e75699e9c497be5de5c097
An "Rick" Truong <104770315+atehvg@users.noreply.github.com>
2026-03-31 12:04:09 +0200

    Add Stefan & Mike

--- data.js
+++ data.js
@@ -319,8 +319,8 @@ const data = {
             Mike: 1,  // 26-02 (& Stefan) door Beau en Jelmer;
         },
         03: {
-            Stefan: 10, // 01-03 (& Mike) door An+Jelmer; // 05-03 (& Mike) door Lars+Michel;  // 05-03 (& Jorik) door Lars+Jasper;  // 05-03 (& Jelmer) door Marijn & Beau; // 09-03 (& Jelmer) door Boyd & Lars; // 16-03 (& Jorik) door Beau & Michel; // 17-03 (& Marijn) door Beau & Ricardo;  // 19-03 (& Jelmer) door Marijn & Mike; // 24-03 (& Mike) door Beau & Ricardo; // 27-03 (& Jelmer) door Beau & Lars;
-            Mike: 4,  // 01-03 (& Stefan) door An+Jelmer; // 05-03 (& Stefan) door Lars+Michel;  // 24-03 (& An) door Beau & Jorik; // 24-03 (& Stefan) door Beau & Ricardo;
+            Stefan: 11, // 01-03 (& Mike) door An+Jelmer; // 05-03 (& Mike) door Lars+Michel;  // 05-03 (& Jorik) door Lars+Jasper;  // 05-03 (& Jelmer) door Marijn & Beau; // 09-03 (& Jelmer) door Boyd & Lars; // 16-03 (& Jorik) door Beau & Michel; // 17-03 (& Marijn) door Beau & Ricardo;  // 19-03 (& Jelmer) door Marijn & Mike; // 24-03 (& Mike) door Beau & Ricardo; // 27-03 (& Jelmer) door Beau & Lars;// 31-03 (& Mike) door Beau+Boyd;
+            Mike: 5,  // 01-03 (& Stefan) door An+Jelmer; // 05-03 (& Stefan) door Lars+Michel;  // 24-03 (& An) door Beau & Jorik; // 24-03 (& Stefan) door Beau & Ricardo;;// 31-03 (& Stefan) door Beau+Boyd
             Jelmer: 7, // 04-03 (& Jasper) door Lars & Ricardo; // 05-03 (& Stefan) door Marijn & Beau; // 09-03 (& Stefan) door Boyd & Lars;  // 19-03 (& Sander) door Lars & Ricardo; // 19-03 (& Stefan) door Marijn & Mike; // 25-03 (& Rick) door Lars + Ricardo; // 27-03 (& Stefan) door Beau & Lars;
             Jasper: 2, // 04-03 (& Jelmer) door Lars & Ricardo; // 11-03 (& Marijn) door Lars & Sander
             Jorik: 2, // 05-03 (& Stefan) door Lars+Jasper;  // 16-03 (& Stefan) door Beau & Michel;
